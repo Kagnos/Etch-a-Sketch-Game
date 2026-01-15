4643b891495b02eac31e8f38135c69ef2204543b
Kagnos <kagcodes@gmail.com>
2026-01-15 10:35:16 -0600

    Grid size button and margins Add button that prompts when clicked Enhance button functionality to only accept a number input from 1 to 100 Add margin style for button Adjust margin style for grid container

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ gridItem.classList.add("grid-item");
 
 for (i = 0; i < 256; i++) {
     gridContainer.appendChild(gridItem.cloneNode(true));
-}
+};
 
 gridContainer.addEventListener("mouseover", (event) => {
     event.target.classList.add("grid-item-active");
 });
 
+const gridSizeButton = document.querySelector("#grid-size-button");
 
+gridSizeButton.addEventListener("click", (event) => {
+    userGridSize = prompt("What size would you like the grid to be? (1-100)");
+    if (userGridSize === null) {
+        alert("test: cancelled");
+    } else if (userGridSize > 0 && userGridSize < 101) {
+        alert("test: correct size");
+    } else {
+        alert("test: incorrect input");
+    }
+});
