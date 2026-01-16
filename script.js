25b351c9c71ce42f0eec3a126e0f96aec94ba9b2
Kagnos <kagcodes@gmail.com>
2026-01-16 08:37:32 -0600

    Reset button and larger grid Add reset button functionality that clears grid while remembering the user inputted grid size Enlarge grid size from 650px by 650px to 1000px by 1000px

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ gridContainer.addEventListener("mouseover", (event) => {
 });
 
 const gridSizeButton = document.querySelector("#grid-size-button");
+let userGridSize;
 
 gridSizeButton.addEventListener("click", (event) => {
     userGridSize = prompt("What size would you like the grid to be? (1-100)");
@@ -20,7 +21,7 @@ gridSizeButton.addEventListener("click", (event) => {
         alert("Cancelled");
     } else if (userGridSize >= 1 && userGridSize <= 100) {
         gridContainer.innerHTML = "";
-        const userGridItemSize = 650 / userGridSize / 650 * 100;
+        const userGridItemSize = 1000 / userGridSize / 1000 * 100;
         for (i = 0; i < (userGridSize * userGridSize); i++) {
                 gridItem.style.cssText = `height: ${userGridItemSize}%; width: ${userGridItemSize}%;`;
                 gridContainer.appendChild(gridItem.cloneNode(true)); 
@@ -29,3 +30,17 @@ gridSizeButton.addEventListener("click", (event) => {
         alert("Please choose a grid size between 1 and 100");
     }
 });
+
+const resetButton = document.querySelector("#reset-button");
+
+resetButton.addEventListener("click", (event) => {
+    if (userGridSize >= 1 && userGridSize <= 100) {
+        gridContainer.innerHTML = "";
+        for (i = 0; i < userGridSize * userGridSize; i++) {
+        gridContainer.appendChild(gridItem.cloneNode(true));
+    }} else {
+        gridContainer.innerHTML = "";
+        for (i = 0; i < 256; i++) {
+        gridContainer.appendChild(gridItem.cloneNode(true));
+    }
+}});
